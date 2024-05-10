9b7a51dd6bd98725a0e565a3346f4533feada8ec
buddhasystem <potekhin@bnl.gov>
2024-05-09 22:32:53 -0400

    Checkpoint

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This repository contains experimental code for interfacing Geant4 functionality
 via Python.
+
+
+# Developer Notes
+
+Most recent examples of Geant4-based application can be found in the tutorials folder
+located on the NPPS GPU host at `/var/data/`
--- setup_example.sh
+++ setup_example.sh
@@ -1,2 +1,7 @@
-export LD_LIBRARY_PATH=/opt/geant4/lib/:/opt/hepmc3/lib/
-export CPATH=/opt/geant4/include/Geant4/:/opt/east/include/:/opt/hepmc3/include/
+# Generic settings of the environment to operate Geant4
+
+export LD_LIBRARY_PATH=/opt/geant4-v11.2.1/lib64
+# /opt/geant4/lib/:/opt/hepmc3/lib/
+
+export CPATH=/opt/geant4-v11.2.1/include/Geant4/
+# /opt/east/include/:/opt/hepmc3/include/
